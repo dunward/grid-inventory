9a64830acbe4ed6748dceca15237f616e7779e91
Elik Song <elik@oxygen.ist>
2024-02-07 22:50:55 +0900

    Add create shadow item begin drag

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,25 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Oxygenist
 {
-    [CreateAssetMenu(menuName = "Create New Item")]
-    public class Item : ScriptableObject
+    public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
-        public string itemName;
-        public Coord2 size;
-        
-        public int Width
+        private Container container;
+
+        [SerializeField]
+        private Coord2 size;
+        public Coord2 Size
         {
-            get => size.x;
-        }
-        public int Height
-        {
-            get => size.y;
+            get => size;
         }
 
-        public ItemRotate rotate;
-        public Sprite sprite;
+        [SerializeField]
+        private Coord2 position;
+        
+        private RectTransform dragTransform;
+        
+        public void UpdatePosition(Coord2 position)
+        {
+            this.position = position;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            dragTransform = CreateShadowItem();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Destroy(dragTransform.gameObject);
+        }
+
+        private RectTransform CreateShadowItem()
+        {
+            var obj = Instantiate(gameObject, transform.parent);
+            obj.GetComponent<CanvasGroup>().alpha = 0.5f;
+
+            return obj.GetComponent<RectTransform>();
+        }
     }
 }
