15d7358933c7db2a01cf1e41df09dcf8dc2d70bd
Elik Song <elik@oxygen.ist>
2024-02-19 21:38:33 +0900

    Edit scale factor caching

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -26,10 +26,13 @@ namespace Oxygenist
         private RectTransform rectTransform;
         private RectTransform dragTransform;
 
+        private float scaleFactor = 0f;
+
         public void Initialize(Container container)
         {
             this.container = container;
             rectTransform.anchoredPosition = new Vector2(position.x * DepotUtility.GRID_UNIT_SIZE, position.y * DepotUtility.GRID_UNIT_SIZE);
+            scaleFactor = InventoryCanvas.Instance.GetCanvas().scaleFactor;
         }
         
         public void UpdatePosition(Coord2 position)
@@ -44,7 +47,7 @@ namespace Oxygenist
 
         public void OnDrag(PointerEventData eventData)
         {
-            dragTransform.anchoredPosition += eventData.delta / InventoryCanvas.Instance.GetCanvas().scaleFactor;
+            dragTransform.anchoredPosition += eventData.delta / scaleFactor;
         }
 
         public void OnEndDrag(PointerEventData eventData)
